d2f90a1957b180b47a64548cb305065fb664d29e
AlmostRenoir <r.augustyn96@gmail.com>
2020-07-18 17:32:14 +0200

    Initial commit

---
+++ src/main/java/almostrenoir/progout/ProgoutApplication.java
@@ -0,0 +1,13 @@
+package almostrenoir.progout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/almostrenoir/progout/ProgoutApplicationTests.java
@@ -0,0 +1,13 @@
+package almostrenoir.progout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
